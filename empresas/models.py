f7575a8900f2d9ec6e4fec6f0ed682695b2ad51f
mateusportal <mateus@portaldeideias.com.br>
2015-03-10 16:19:40 -0300

    Inicio da classe Empresa do objeto empresas

--- empresas/models.py
+++ empresas/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
 class Empresa(models.Model):
+	logo = models.CharField(default='semfoto.png', max_length=100, blank=True, null=True)
+	razao_social = models.CharField(max_lenght=100, blank=False, null=False)
+	nome_fantasia = models.CharField(max_lenght=100, blank=False, null=False)
+	cnpj = models.CharField(db_index=True, max_length=18, blank=True, null=True)
+	endereco = models.CharField(max_lenght=100, blank=False, null=False)
+	telefone = models.CharField(max_lenght=15, blank=False, null=False)
 
 class Usuario(models.Model):
