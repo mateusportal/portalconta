855218e397f0ddc68be5bcb985a98cc923510dd1
mateusportal <mateus@portaldeideias.com.br>
2015-03-10 14:51:02 -0300

    Preparando aquivos para habilitar o admin do django

--- contas/admin.py
+++ contas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from contas.models import ContaEntrada,ContaSaida,Vinculo,Categoria,Grupo,SubGrupo
 
-# Register your models here.
+admin.site.register(ContaEntrada)
+admin.site.register(ContaSaida)
+admin.site.register(Vinculo)
+admin.site.register(Categoria)
+admin.site.register(Grupo)
+admin.site.register(SubGrupo)
