3cca0bc7d6bbbf96a2c11855e927ffd67ffcab2f
edsonlb <ti_edson@yahoo.com.br>
2015-03-11 16:52:00 -0300

    Atualizando settings

--- contas/models.py
+++ contas/models.py
@@ -1,13 +1,5 @@
 from django.db import models
 
-class ContaEntrada(models.Model):
+#class ContaEntrada(models.Model):
 
-class ContaSaida(models.Model):
-
-class Vinculo(models.Model):
-
-class Categoria(models.Model):
-
-class Grupo(models.Model):
-
-class SubGrupo(models.Model):
+#class Vinculo(models.Model):
--- pc/urls.py
+++ pc/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pc.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'core.views.index', name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
