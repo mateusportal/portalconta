836b51f4316eb4d1508b6e747f15a2ddb36fabcf
mateusportal <mateus@portaldeideias.com.br>
2015-03-13 16:00:47 -0300

    Preparando arquivos forms.py

--- empresas/forms.py
+++ empresas/forms.py
@@ -1,1 +1,12 @@
 from django import forms
+from empresas.models import Pessoa, Empresa
+
+class PessoaForm(forms.ModelForm):
+
+    class Meta:
+        model = Pessoa
+
+class EmpresaForm(forms.ModelForm):
+
+    class Meta:
+        model = Empresa
