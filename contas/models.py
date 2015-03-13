9579e8a079262978b1bf1def0e5cd5d9aebb5ad8
mateusportal <mateus@portaldeideias.com.br>
2015-03-13 13:49:22 -0300

    finalizando classes

--- contas/models.py
+++ contas/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # = models.CharField(max_length=250, blank=True, null=True)
 
 class Caixa(models.Model):
-	pessoa_id = models.ForeignKey(Pessoa,related_name="pessoa_id")
+	pessoa_id = models.ForeignKey(Pessoa,related_name="caixa_pessoa_id")
 	valor_bruto = models.DecimalField(max_digits=7, decimal_places=2)
 	data_vencimento = models.DateTimeField(auto_now=False, auto_now_add=False)
 	data_pagamento = models.DateTimeField(auto_now=False, auto_now_add=False)
@@ -17,16 +17,31 @@ class Caixa(models.Model):
 	data_cadastro = models.DateTimeField(auto_now=False, auto_now_add=True)
 	data_alteracao = models.DateTimeField(auto_now=True, auto_now_add=True)
 	ativo = models.CharField(default="SIM", max_length=50, blank=False, null=False)
-	usuario_id = models.ForeignKey(Pessoa,related_name="pessoa_id")
-	empresa_id = models.ForeignKey(Empresa,related_name="empresa_id")
+	usuario_id = models.ForeignKey(Pessoa,related_name="caixa_usuario_id")
+	empresa_id = models.ForeignKey(Empresa,related_name="caixa_empresa_id")
 	observacao = models.CharField(max_length=250, blank=True, null=True)
 	tags = models.CharField(max_length=250, blank=True, null=True)
 	data_boleto = models.DateTimeField(auto_now=False, auto_now_add=False)
 	processo = models.CharField(max_length=250, blank=True, null=True)
-	categoria_id = models.ForeignKey(Sistema,related_name="categoria_id")
-	grupo_id = models.ForeignKey(Sistema,related_name="grupo_id")
-	subgrupo_id = models.ForeignKey(Sistema,related_name="subgrupo_id")
+	categoria_id = models.ForeignKey(Sistema,related_name="caixa_categoria_id")
+	grupo_id = models.ForeignKey(Sistema,related_name="caixa_grupo_id")
+	subgrupo_id = models.ForeignKey(Sistema,related_name="caixa_subgrupo_id")
 
 
-#class Vinculo(models.Model):
+class Vinculo(models.Model):
+	caixa_pai = models.ForeignKey(Caixa,related_name="vinculo_caixa_pai_id")
+	caixa_filho = models.ForeignKey(Caixa,related_name="vinculo_caixa_filho_id")
+	observacao = models.CharField(max_length=250, blank=True, null=True)
+	rastreador = models.CharField(max_length=250, blank=True, null=True)
+	tags = models.CharField(max_length=250, blank=True, null=True)
+	empresa_id = models.ForeignKey(Caixa,related_name="vinculo_empresa_id")
+
+class Sistema(models.Model):
+	tipo = models.CharField(max_length=50, blank=True, null=True)
+	nome = models.CharField(max_length=150, blank=True, null=True)
+	empresa_id = models.ForeignKey(Caixa,related_name="sistema_empresa_id")
+	tags = models.CharField(max_length=250, blank=True, null=True)
+	descricao = models.CharField(max_length=250, blank=True, null=True)
+	data_cadastro = models.DateTimeField(auto_now=False, auto_now_add=True)
+	ativo = models.CharField(default="SIM", max_length=50, blank=False, null=False)
 
