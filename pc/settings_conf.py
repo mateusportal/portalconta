da0f7fe4bb57ed37cc293161257763e62b85ecf5
edsonlb <ti_edson@yahoo.com.br>
2015-03-13 17:15:23 -0300

    Arquivo Settings_conf

---
+++ pc/settings_conf.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+CONF_SECRET_KEY = 'hjf2gfs4a#^d=b&#k9p09g%lu=*sfkc@=zv+4#m(3j26=7b5a&'
+CONF_EMAIL_HOST = 'smtp.gmail.com'
+CONF_EMAIL_HOST_USER = 'nao_responda@celuladigital.com.br'
+CONF_EMAIL_HOST_PASSWORD = 'entercds'
+CLOUDINARY_CLOUD_NAME = "portal-de-ideias",
+CLOUDINARY_API_KEY = "185688679931993",
+CLOUDINARY_API_SECRET = "k83jqr8DeiDfYySVgjoOIQw5K50"
