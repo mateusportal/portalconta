3252bb8236296a7c866391cc94e8141052866023
edsonlb <ti_edson@yahoo.com.br>
2015-03-13 16:15:41 -0300

    Elaborando Cadastro

--- core/views.py
+++ core/views.py
@@ -5,3 +5,12 @@ def index(request):
 
 def login(request):
     return render(request,'index/login.html')
+
+def valida_login(request):
+    return render(request,'index/login.html')
+
+def cadastro(request):
+    return render(request,'index/cadastro.html')
+
+def valida_cadastro(request):
+    return render(request,'index/cadastro.html')
--- empresas/forms.py
+++ empresas/forms.py
@@ -1,1 +1,17 @@
 from django import forms
+from empresas.models import Usuario
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length='200', required=True)
+    password = forms.CharField(widget=forms.PasswordInput, required=True)
+
+class UsuarioForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(UsuarioForm, self).__init__(*args, **kwargs)
+
+        self.fields['last_login'].widget = forms.HiddenInput()
+        self.fields['date_joined'].widget = forms.HiddenInput()
+
+    class Meta:
+        model = Usuario
