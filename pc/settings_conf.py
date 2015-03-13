e6ee7eedb777375924d01caeb0435e9df320d3f9
edsonlb <ti_edson@yahoo.com.br>
2015-03-13 17:26:14 -0300

    teste

--- pc/settings_conf.py
+++ pc/settings_conf.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+# NAO ENVIAR PARA GIT!
 CONF_SECRET_KEY = 'hjf2gfs4a#^d=b&#k9p09g%lu=*sfkc@=zv+4#m(3j26=7b5a&'
 CONF_EMAIL_HOST = 'smtp.gmail.com'
 CONF_EMAIL_HOST_USER = 'nao_responda@celuladigital.com.br'
